2bdb4ffa90ca6d0d8d94c146ed84ab1d01dc24ce
Rounaksondawale <149842957+Rounaksondawale@users.noreply.github.com>
2026-02-22 12:02:50 +0530

    Refactor MySQL connection settings
    Updated MySQL connection environment variables and added port configuration.

--- fairbase_Messaging/server.js
+++ fairbase_Messaging/server.js
@@ -19,10 +19,13 @@ admin.initializeApp({
 /* ---------- MySQL Connection ---------- */
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 /* ---------- Send FCM ---------- */
